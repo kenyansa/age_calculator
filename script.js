b51940bd941dd1369d316c394cb315b652f7f981
Kenyansa <kenyansajustine@gmail.com>
2023-07-09 23:38:28 +0300

    add control statements

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function calculateAge(){
 
     let d2 = today.getDate();
     let m2 = today.getMonth ()+1;
-    let y2 = today.getFullYear() 
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+    y3 = y2 - y1;
+
+    if (m2>=m1){
+        m3 = m2 - m1;
+    }else{
+        y3--;
+        m3 = 12 + m2 - m1
+    }
+
+    if(d2>=d1){
+        d3= d2 - d1;
+    }else{
+
+    }
 
 }
